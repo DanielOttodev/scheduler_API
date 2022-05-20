fda3a073669a9fb0fe96a0a97a951259e1dc4548
otto281 <otto_281@hotmail.com>
2022-05-20 18:37:05 +1000

    initial commit

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 let pool = mysql.createPool({
-  host: "zenlydbfree.c2gvp4xdttbs.ap-southeast-2.rds.amazonaws.com",
-  user: "admin",
-  password: "1Booster23",
-  database: "zenlydbfree",
+  host: "",
+  user: "",
+  password: "",
+  database: "",
 });
 module.exports = {
   pool,
